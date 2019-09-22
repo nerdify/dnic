5b618a98dbfda427d64f9f0a8fc97ecc2d25898e
Gustavo Chavarria <gchava92@gmail.com>
2019-09-22 11:28:32 -0600

    WIP: regExp

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-export const dnic = (dni) => {
-	//check entries
+export const dnic = dni => {
+	console.log('dni: ', dni);
 
+	//([0-6][0-9]{2})-([0-2][0-9]|3[0-1])(0[1-9]|1[0-2])([0-9]{2})-([0-9]{4}[A-X])
+	const regExp = RegExp('([0-6][0-9]{2})-([0-2][0-9]|3[0-1])(0[1-9]|1[0-2])([0-9]{2})-([0-9]{4}[A-X])');
+
+	return regExp.test(dni);
 };
