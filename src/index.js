d6c1ac124df4156aa27f105f79ccd488eb4dc947
tavo <gchava92@gmail.com>
2019-09-25 18:31:13 -0600

    WIP: dnicWithDateValidation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 export const dnic = dni => {
-	// ([0-6][0-9]{2})-([0-2][0-9]|3[0-1])(0[1-9]|1[0-2])([0-9]{2})-([0-9]{4}[A-X])
 	const regex = /([0-6][d]{2})-([0-2][d]|3[0-1])(0[1-9]|1[0-2])([d]{2})-([d]{4}[A-X])/;
 
 	return regex.test(dni);
 };
 
 export const dnicWithDateValidation = dni => {
-	if (!dni) {
-		return null;
+	if (!dni || !dnic(dni)) {
+		return false;
 	}
+
+	const date = dni.split('-')[1];
+
+	const day = date.slice(0,2);
+	const month = date.slice(2, 4);
+	const year = date.slice(4,6);
+
+	const newDate = new Date(year, (month -1), day);
+	
+	return !!newDate;
 };
