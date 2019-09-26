a425c41869321db717f6fb11a8625dd029c88165
Hosmel Quintana <hosmelq@gmail.com>
2019-09-26 14:21:02 -0600

    fix regex

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const dnic = dni => {
     return false;
   }
 
-  const regex = /([0-6][d]{2})-([0-2][d]|3[0-1])(0[1-9]|1[0-2])([d]{2})-([d]{4}[A-X])/;
+  const regex = /^([0-6][\d]{2})-([0-2][\d]|3[0-1])(0[1-9]|1[0-2])([\d]{2})-([\d]{4}[A-X])$/;
 
   return regex.test(dni);
 };
