d3117d06053f3ecc381df8e40454be32f9f72b37
tavo <gchava92@gmail.com>
2019-09-27 18:41:34 -0600

    WIP: adding municipalitis of nicaragua

--- src/municipalities.js
+++ src/municipalities.js
@@ -1,3 +1,52 @@
+const boaco = [
+  {'361': 'Boaco'},
+  {'362': 'Camoapa'},
+  {'363': 'Santa Lucía'},
+  {'364': 'San José Del Remate'},
+  {'365': 'San Lorenzo'},
+  {'366': 'Teustepe'}
+];
+
+const carazo = [
+  {'041': 'Jinotepe'},
+  {'042': 'Diriamba'},
+  {'043': 'San Marcos'},
+  {'044': 'Santa Teresa'},
+  {'045': 'Dolores'},
+  {'046': 'La Paz Carazo'},
+  {'047': 'El Rosario'},
+  {'048': 'La Conquista'}
+];
+
+const chinandega = [
+  {'081': 'Chinandega'},
+  {'082': 'Corinto'},
+  {'083': 'El Realejo'},
+  {'084': 'Chichigalpa'},
+  {'085': 'Posoltega'},
+  {'086': 'El Viejo'},
+  {'087': 'Puerto Morazán'},
+  {'088': 'Somotillo'},
+  {'089': 'Villa Nueva'},
+  {'090': 'Santo Tomás del Norte'},
+  {'091': 'Cinco Pinos'},
+  {'092': 'San Francisco Del Norte'},
+  {'093': 'San Pedro Del Norte'}
+];
+
+const chontales = [
+  {'121': 'Juigalpa'},
+  {'122': 'Acoyapa'},
+  {'123': 'Santo Tomás'},
+  {'124': 'Villa Sandino'},
+  {'125': 'San Pedro de Lóvago'},
+  {'126': 'La Libertad'},
+  {'127': 'Santo Domingo'},
+  {'128': 'Comalapa'},
+  {'129': 'San Francisco Cuapa'},
+  {'130': 'El Coral'}
+];
+
 const managua = [
   {'001': 'Managua'},
   {'002': 'San Rafael Del Sur'},
@@ -9,14 +58,11 @@ const managua = [
 
 const leon = [{'281': 'Leon'}];
 
-const chinandega = [
-  {'081': 'Chinandega'},
-  {'082': 'Corinto'},
-  {'083': 'El Realejo'},
-  {'084': 'Chichigalpa'},
-  {'085': 'Posoltega'},
-  {'086': 'El Viejo'},
-  {'087': 'Puerto Morazán'}
+export const municipalities = [
+  ...boaco,
+  ...carazo,
+  ...chinandega,
+  ...chontales,
+  ...leon,
+  ...managua
 ];
-
-export const municipalities = [...managua, ...leon, ...chinandega];
