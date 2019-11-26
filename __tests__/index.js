432c64e64d69d57ce106c6a675a0c37fbfaa76ad
GustavoChavarria <gchava92@gmail.com>
2019-11-26 15:22:25 -0600

    Refactors

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,18 +1,22 @@
 import test from 'ava';
 
-import dnic, {dnicWithDateValidation} from '../src';
+import dnic, {validate, validateDate} from '../src';
 
 const VALID_DNI = '001-280592-0025N';
 const INVALID_DATE = '001-310292-0000N';
 
+test('should pass a valid dni (default)', t => {
+  t.true(dnic.isValid(VALID_DNI));
+});
+
 test('should pass a valid dni', t => {
-  t.true(dnic(VALID_DNI));
+  t.true(validate(VALID_DNI));
 });
 
 test('valid dni (date)', t => {
-  t.true(dnicWithDateValidation(VALID_DNI));
+  t.true(validateDate(VALID_DNI));
 });
 
 test('valid a wrong date', t => {
-  t.false(dnicWithDateValidation(INVALID_DATE));
+  t.false(validateDate(INVALID_DATE));
 });
